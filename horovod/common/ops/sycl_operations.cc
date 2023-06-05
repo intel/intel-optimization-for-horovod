3bf09d223f9d0e568787aae6eab78d45dc098a84
zhangxiaoli73 <cherry.zhang@intel.com>
2023-06-05 15:01:21 +0800

    Move scaled kernel to sycl kernel file (#202)

--- horovod/common/ops/sycl_operations.cc
+++ horovod/common/ops/sycl_operations.cc
@@ -16,6 +16,7 @@
 #include "../hashes.h"
 #include "../message.h"
 #include "gpu_operations.h"
+#include "sycl/sycl_kernels.h"
 
 #include <algorithm>
 #include <thread>
@@ -130,27 +131,6 @@ public:
     }
   }
 
-  template <typename T, typename TS>
-  void ScaleBufferSyclImpl(const T* input, T* output, int64_t num_elements,
-                           TS scale_factor, gpuStream_t& stream) {
-    const int wg_size =
-        stream->get_device()
-            .get_info<sycl::info::device::max_work_group_size>();
-    const int num_workgroups = (num_elements + wg_size - 1) / wg_size;
-    stream->submit([&](sycl::handler& h) {
-      sycl::range<1> global(num_workgroups * wg_size);
-      sycl::range<1> local(wg_size);
-      h.parallel_for<ScaleBufferSyclKernel<T, TS>>(
-          sycl::nd_range<1>(global, local), [=](sycl::nd_item<1> it) {
-            auto id = it.get_global_linear_id();
-            if (id >= num_elements)
-              return;
-
-            output[id] = scale_factor * input[id];
-          });
-    });
-  }
-
   void StreamCreate(const TensorTableEntry& e, gpuStream_t& stream) {
     auto org_q = e.context->SYCLQueue();
     auto property_list = sycl::property_list{sycl::property::queue::in_order()};
@@ -187,52 +167,8 @@ public:
   void ScaleBufferImpl(const void* fused_input_data, void* buffer_data,
                        int64_t num_elements, double scale_factor,
                        DataType dtype, gpuStream_t& stream) {
-    auto float_scale_factor = (float)scale_factor;
-    switch (dtype) {
-    case HOROVOD_UINT8:
-      ScaleBufferSyclImpl((const uint8_t*)fused_input_data,
-                          (uint8_t*)buffer_data, num_elements,
-                          float_scale_factor, stream);
-      break;
-    case HOROVOD_INT8:
-      ScaleBufferSyclImpl((const int8_t*)fused_input_data, (int8_t*)buffer_data,
-                          num_elements, float_scale_factor, stream);
-      break;
-    case HOROVOD_INT32:
-      ScaleBufferSyclImpl((const int32_t*)fused_input_data,
-                          (int32_t*)buffer_data, num_elements,
-                          float_scale_factor, stream);
-      break;
-    case HOROVOD_INT64:
-      ScaleBufferSyclImpl((const int64_t*)fused_input_data,
-                          (int64_t*)buffer_data, num_elements,
-                          float_scale_factor, stream);
-      break;
-    case HOROVOD_FLOAT16:
-      ScaleBufferSyclImpl((const sycl::half*)fused_input_data,
-                          (sycl::half*)buffer_data, num_elements,
-                          (sycl::half)float_scale_factor, stream);
-      break;
-#if HAVE_DPCPP
-    case HOROVOD_BFLOAT16:
-      using bfloat16 = sycl::ext::oneapi::bfloat16;
-      ScaleBufferSyclImpl((const bfloat16*)fused_input_data,
-                          (bfloat16*)buffer_data, num_elements,
-                          (bfloat16)float_scale_factor, stream);
-      break;
-#endif // HAVE_DPCPP
-    case HOROVOD_FLOAT32:
-      ScaleBufferSyclImpl((const float*)fused_input_data, (float*)buffer_data,
-                          num_elements, float_scale_factor, stream);
-      break;
-    case HOROVOD_FLOAT64:
-      ScaleBufferSyclImpl((const double*)fused_input_data, (double*)buffer_data,
-                          num_elements, scale_factor, stream);
-      break;
-    default:
-      throw std::logic_error("Type " + DataType_Name(dtype) +
-                             " not supported by ScaleBufferSyclImpl.");
-    }
+    ScaleBufferSYCLImpl(fused_input_data, buffer_data, num_elements,
+                        scale_factor, dtype, stream);
   }
 
 private:
